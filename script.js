1fc2956a462fb37661a9aab9ffba343daa70b7bc
Ritik Sapat <118529770+RitikSapat02@users.noreply.github.com>
2023-04-17 23:05:19 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+const counter = document.querySelector(".counter");
+
+const barElement = document.querySelector(".loading-bar-front");
+
+let idx = 0;
+updateNum();
+
+function updateNum() {
+  counter.innerText = idx + "%";
+  barElement.style.width = idx + "%";
+  idx++;
+  if (idx < 101) {
+    setTimeout(updateNum, 20);
+  }
+}
